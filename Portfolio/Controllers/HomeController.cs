3af3bbb6af165ec0156ca9bce7051b6ca34d90ee
UnBottino <unbottino@gmail.com>
2024-05-07 11:20:18 +0100

    Created fading navbar. Skills on hover improved. Added scrolling cv image and link. Fixed project boxes to be more responsive. Added contact form.

--- Portfolio/Controllers/HomeController.cs
+++ Portfolio/Controllers/HomeController.cs
@@ -3,21 +3,49 @@ using Portfolio.Models;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Mail;
+using System.Web.Helpers;
+using System.Xml.Linq;
+using MailKit;
 
 namespace Portfolio.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment environment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment hostEnvironment)
         {
             _logger = logger;
+            environment = hostEnvironment;
         }
 
         public IActionResult Index()
         {
+            var skillsFilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\lib\\skills.txt");
+            var skillsFormattedFilePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\lib\\skills_formatted.txt");
+
+            if (System.IO.File.Exists(skillsFilePath))
+            {
+                var skills = System.IO.File.ReadLines(skillsFilePath).ToArray();
+                var skills_formatted = System.IO.File.ReadLines(skillsFormattedFilePath).ToArray();
+
+                ViewBag.Skills = skills;
+                ViewBag.SkillsFormatted = skills_formatted;
+            }
+            else
+            {
+                ViewBag.Skills = "File not found.";
+                ViewBag.SkillsFormatted = "File not found.";
+            }
             return View();
+        }       
+
+        public FileResult AdrianCollins_2024()
+        {
+            var filepath = Path.Combine(environment.WebRootPath, "files", "AdrianCollins_CV(2024).pdf");
+            byte[] FileBytes = System.IO.File.ReadAllBytes(filepath);
+            return File(FileBytes, "application/pdf");
         }
 
         public IActionResult About()
@@ -42,38 +70,40 @@ namespace Portfolio.Controllers
         {
             if (ModelState.IsValid)
             {
-                var myEmail = new MailAddress("unbottino@gmail.com", "Adrian Collins");
-                const string myPassword = "sjxcjpafyjluhkfy";
+                string myEmail = "adriancollins59@live.com";
+                const string myPassword = "T2qv9jB8kfJaZhkpgANsQZb";
                 const string sub = "Portfolio Email";
 
-                var body = model.FirstName + " ";
-                body += model.Surname + "\r\n";
-                body += model.Email + "\r\n";
-                body += model.PhoneNumber + "\r\n";
-                body += model.Message;
+                var usersAddress = new MailAddress(model.Email);
+                var body = model.Message;
 
                 try
                 {
-                    var smtp = new SmtpClient
-                    {
-                        Host = "smtp.gmail.com",
-                        Port = 587,
-                        EnableSsl = true,
-                        DeliveryMethod = SmtpDeliveryMethod.Network,
-                        UseDefaultCredentials = false,
-                        Credentials = new NetworkCredential(myEmail.Address, myPassword)
-                    };
-                    using var message = new MailMessage(myEmail, myEmail)
-                    {
-                        Subject = sub,
-                        Body = body
-                    };
-                    smtp.Send(message);
+                    MailMessage mailMessage = new MailMessage();
+                    mailMessage.From = usersAddress;
+                    mailMessage.To.Add(new MailAddress(myEmail));
+                    mailMessage.IsBodyHtml = true;
+                    mailMessage.Subject = sub;
+                    mailMessage.Body = body;
+                    mailMessage.BodyEncoding = System.Text.Encoding.UTF8;
+                    mailMessage.SubjectEncoding = System.Text.Encoding.UTF8;
+
+                    SmtpClient client = new SmtpClient();
+                    
+                    NetworkCredential su = new NetworkCredential(myEmail, myPassword);
+                    client.Host = "smtp-mail.outlook.com";
+                    client.Port = 587;
+                    client.Credentials = su;
+                    client.Send(mailMessage);
+
+                    client.Send(mailMessage);
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
+
+                Console.WriteLine("it worked");
 
                 return Json(new { Success = true, redirectToUrl = Url.Action("About", "Home") });
             }
--- Portfolio/wwwroot/js/site.js
+++ Portfolio/wwwroot/js/site.js
@@ -1,4 +1,112 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//Change Navbar on scroll (Navbar)
+$(function () {
+	var nowScrollTop = 0;
+	$(window).on("scroll", function () {
+		nowScrollTop = $(this).scrollTop();
 
-// Write your JavaScript code.
+		if (nowScrollTop > 0) {
+			$(".navbar, #logo-text, .nav-link, .navbar-toggler-icon i").css('color', '#d2d2d2');
+			$(".navbar").addClass('scrolled');
+			$(".navbar-collapse").removeClass('show');
+		} else {
+			$(".navbar, #logo-text, .nav-link, .navbar-toggler-icon i").css('color', '');
+			$(".navbar").removeClass('scrolled');
+		}
+	});
+});
+
+$(".navbar-toggler").on("click", function () {
+	$(".navbar, #logo-text, .nav-link, .navbar-toggler-icon i").css('color', '#d2d2d2');
+	$(".navbar").addClass('scrolled');
+});
+
+//Navbar navigation
+$('.nav-link').on("click", function () {
+	$(".navbar-collapse").removeClass('show');
+});
+
+$("#nav-about").on("click", function () {
+	$([document.documentElement, document.body]).animate({
+		scrollTop: $("#about").offset().top - $("#navbar").outerHeight(true)
+	}, 100);
+});
+
+$("#nav-skills").on("click", function () {
+	$([document.documentElement, document.body]).animate({
+		scrollTop: $("#skills").offset().top - $("#navbar").outerHeight(true)
+	}, 100);
+});
+
+$("#nav-experience").on("click", function () {
+	$([document.documentElement, document.body]).animate({
+		scrollTop: $("#experience").offset().top - $("#navbar").outerHeight(true)
+	}, 100);
+});
+
+$("#nav-projects").on("click", function () {
+	$([document.documentElement, document.body]).animate({
+		scrollTop: $("#projects").offset().top - $("#navbar").outerHeight(true)
+	}, 100);
+});
+
+$("#nav-contact").on("click", function () {
+	$([document.documentElement, document.body]).animate({
+		scrollTop: $("#contact").offset().top - $("#navbar").outerHeight(true)
+	}, 100);
+});
+
+//Contact Modal
+$('a#submit-modal').on('click', function () {
+	$.ajax({
+		url: "/Home/Contact",
+		type: "POST",
+		dataType: "json",
+		data: $("#contact-form").serialize(),
+		contentType: "application/x-www-form-urlencoded; charset=utf-8",
+		success: function (response) {
+			if (response.success) {
+
+			} else {
+
+			}
+		},
+		error: function (response) {
+
+		}
+	});
+});
+
+//Scroll to top (Footer)
+$("#scrollToTop").on("click", function () {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0;
+});
+
+const sections = document.querySelectorAll("section[id]");
+
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+	// Get current scroll position
+	let scrollY = window.scrollY;
+
+	// Now we loop through sections to get height, top and ID values for each
+	sections.forEach(current => {
+		const sectionHeight = current.offsetHeight;
+		const sectionTop = current.offsetTop - $("#navbar").outerHeight(true) -30;
+		sectionId = current.getAttribute("id");
+
+		/*
+		- If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+		- To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+		*/
+		if (
+			scrollY > sectionTop &&
+			scrollY <= sectionTop + sectionHeight
+		) {
+			document.querySelector("li#nav-" + sectionId).classList.add("active");
+		} else {
+			document.querySelector("li#nav-" + sectionId).classList.remove("active");
+		}
+	});
+}
